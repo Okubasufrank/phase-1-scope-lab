a09263f16a586111da6a98a799c354444fe489ea
Okubasufrank <franklineokubasu51@gmail.com>
2022-06-24 00:18:49 +0300

    commit tree 867d021a4542c83390501ef0b8261d6cc5cd368d

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = "bob";
+const error = "test";
+
+function upperCaseCustomerName(){
+
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+setBestCustomer()
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+overwriteBestCustomer()
+
+function changeLeastFavoriteCustomer(){
+    error = "no error"
+}
+
+changeLeastFavoriteCustomer()
